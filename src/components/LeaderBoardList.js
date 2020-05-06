0128838f5174cb692c28648a902a1752e5064444
Dylan Irmak <dylan.irmak@azure.lloydsbank.com>
2020-05-06 14:05:57 +0100

    added leaderboard info

--- src/components/LeaderBoardList.js
+++ src/components/LeaderBoardList.js
@@ -1,21 +1,26 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
 
-class LeaderBoardList extends Component {
+export default class LeaderBoardList extends Component {
+    
+    
     render() {
         const { user } = this.props
         return (
             <div className='leaderboard-box'>   
-                <h3>{user.id}</h3>
+                <h5>User: {user.id}</h5>
+                <div className='author'>
+                    <img  
+                        src={user.avatarURL}
+                        alt={`Avatar of ${user.id}`}
+                        className='author-avatar'/>
+                </div>
+                <div className='questions-asked'>
+                    <span>Questions asked: {user.questions.length}</span>
+                </div>
+                <div className='questions-answered'>
+                    <span>Questions answered: {Object.keys(user.answers).length}</span>
+                </div>
             </div>
         )
     }
 }
-
-function mapStateToProps({ questions }) {
-    return {
-        questions
-    }
-}  
-
-export default connect(mapStateToProps)(LeaderBoardList)
